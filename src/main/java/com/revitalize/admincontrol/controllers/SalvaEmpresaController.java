a4860222fd378fd60db057bb9cb1db6b7d357ea6
Glauber Pinheiro <glauberspinheiro@gmail.com>
2022-10-03 20:14:58 -0300

    Atualizando repositorio.

--- src/main/java/com/revitalize/admincontrol/controllers/SalvaEmpresaController.java
+++ src/main/java/com/revitalize/admincontrol/controllers/SalvaEmpresaController.java
@@ -1,13 +1,17 @@
 package com.revitalize.admincontrol.controllers;
 
 import com.revitalize.admincontrol.models.AdmEmpresaModel;
+import com.revitalize.admincontrol.models.AdmUsuarioModel;
 import com.revitalize.admincontrol.repository.AdmEmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.UUID;
 
 @Controller
 public class SalvaEmpresaController {
@@ -19,6 +23,7 @@ public class SalvaEmpresaController {
 
         return "registercompany";
     }
+
 
     @RequestMapping(value="/addcompany", method=RequestMethod.POST)
     public String formCompany(AdmEmpresaModel admEmpresaModel) {
